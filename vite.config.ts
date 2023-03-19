f0ee29af55c76db6e60933ecb08dd2d963149734
unknown <miro.a.salo@gmail.com>
2023-03-19 13:31:20 +0200

    setup gh-pages1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fitness-project/',
+  base: '/fitness-site/',
   plugins: [react()],
 })
